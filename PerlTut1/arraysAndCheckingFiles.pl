993a1b12ab41ba63cb745dbcdedc4a2e5c538a5b
Mohd Ikhwan Mohd Aljeffri <ikkialjeffri@gmail.com>
2016-10-09 21:07:26 -0400

    added array for checking whether file exists. finished tutorial for arrays and checking files

--- PerlTut1/arraysAndCheckingFiles.pl
+++ PerlTut1/arraysAndCheckingFiles.pl
@@ -2,14 +2,28 @@
 use strict;
 use warnings;
 
-sub main{
-	#-f means check if this file exist or not
-	if(-f 'C:\tutorial\perl\projects\logo.png'){
-		print "Found file\n";
+#this turns off output buffering, so our print statements gets executed immediately
+$|=1;
+
+sub main {
+
+	#array starts with the @ sign
+	my @files = (
+		'/Users/ikkialjeffri/git/PerlLessons/PerlTut1/poster.png',
+		'/Users/ikkialjeffri/git/PerlLessons/PerlTut1/membership.html',
+		'/Users/ikkialjeffri/git/PerlLessons/PerlTut1/missing.txt',
+	);
+	
+	foreach my $file (@files) {
+		#-f means check if this file exist or not
+		if ( -f $file ) {
+			print "Found file: $file\n";
+		}
+		else {
+			print "File not found $file\n";
+		}
 	}
-	else{
-		print "File not found\n";
-	}
+	
 }
 
 main();
